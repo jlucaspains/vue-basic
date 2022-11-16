98fb0dd41366c7e31c1d3e0845802d8292cd6dc8
Lucas Pains <jlucaspains@gmail.com>
2022-11-15 16:06:08 -0800

    One more try to fix function

--- api/function_app.py
+++ api/function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 # Get started by running the following code to create a function using a HTTP trigger.
 
-@app.function_name(name="HttpTrigger1")
+@app.function_name(name="lpains_hello")
 @app.route(route="hello")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
      logging.info('Python HTTP trigger function processed a request.')
